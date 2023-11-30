60f964a96f116a9233d86c04f7289ea7c6936e0c
Dave2002 <Paulalfrex@gmail.com>
2023-11-30 01:21:56 +0100

    Trying to use TKinter again

--- logic2.0.py
+++ logic2.0.py
@@ -1,134 +1,291 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel
-from PyQt5.QtCore import Qt
+import asyncio
+import multiprocessing
+import random
+import time
+import tkinter as tk
+from tkinter import ttk, PhotoImage, font
+import threading
+from subprocess import call
+import pygame
 
-class GameSelector(QWidget):
+#import led
+from legacy.armBomb import armBomb
+
+# Led shit adden.
+# Flage und Bunker bauen.
+pygame.init()
+pygame.mixer.init()
+
+
+def codeGen(x):
+    var = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D"]
+    tmp = []
+    for g in range(0, x):
+        tmp.append(var[random.randint(0, len(var) - 1)])
+    return "".join(tmp)
+
+
+class logicWindow:
+
     def __init__(self):
-        super().__init__()
+        self.mp = None
+        self.stop = False
+        self.blinker = None
+        self.timeLable1 = None
+        self.timeLable1 = None
+        self.counter1 = False
+        self.counter2 = False
+        # arm/defu labels
+        self.timerLable = None
+        self.armed = None
+        self.infoLable = None
+        self.armCode = None
+        self.defCode = None
+        self.armTries = 3
+        self.bomTries = 3
+        self.inputLock = False
+        self.versucheLable = None
+        self.inputLable = None
+        self.p1 = None
+        self.input = []
+        self.fails = 1
+        self.exitCode = 6969
+        self.p1Sop = False
+        # selection logic
 
-        self.game_modes = ["Bomb", "Flag", "Bunker"]
-        self.difficulties = ["Easy", "Medium", "Hard"]
-        self.times = ["5min", "10min", "15min"]
+        self.pressedKeys = []
+        self.hoehe = [120, 220, 320]
+        self.modie = ["Bombe", "Bunker", "Flage"]
+        self.diffs = ["Easy", "Medium", "Hard"]
+        self.times = [1, 10, 15]
+        self.selectedGame = None
+        self.selectedDiff = None
+        self.selectedTime = None
+        self.current = 0
+        self.selection = -1
+        self.game = None
+        self.isInGame = False
 
-        self.selection_index = 0
+        self.root = tk.Tk()
+        self.root.attributes("-fullscreen", True)
+        self.root.focus_force()
+        self.ev1 = self.root.bind("<KeyPress>", self.keydown)
+        self.ev2 = self.root.bind("<KeyRelease>", self.keyup)
+        self.root.title("Foxy´s Bombe")
+        self.root.geometry("800x480")
+        self.root.configure(background="black")
+        tk.Label(self.root, name="text", text="Spiel4:", bg="black", fg="green", font=("Ubuntu", 50)).pack()
 
-        self.selected_game_mode = None
-        self.selected_difficulty = None
-        self.selected_time = None
+        tk.Label(self.root, name="lable1", text="1:Bombe", bg="black", fg="green", font=("Ubuntu", 45)).place(x=150,
+                                                                                                              y=120)
+        tk.Label(self.root, name="lable2", text="2:Bunker", bg="black", fg="green", font=("Ubuntu", 45)).place(x=150,
+                                                                                                               y=220)
+        tk.Label(self.root, name="lable3", text="3:Flage", bg="black", fg="green", font=("Ubuntu", 45)).place(x=150,
+                                                                                                              y=320)
 
-        self.init_ui()
+        tk.Label(self.root, name="dont1", text="Rot: Zurück", fg="green", bg="black", font=("Ubuntu", 30)).place(
+            relx=0.0, rely=1.0, anchor='sw')
+        tk.Label(self.root, name="dont2", text="Blau: Bestätigen", fg="green", bg="black", font=("Ubuntu", 30)).place(
+            relx=1.0, rely=1.0, anchor='se')
 
-    def init_ui(self):
-        layout = QVBoxLayout()
+        self.selectLable = tk.Label(self.root, text="<--", bg="black", fg="green", font=("calibri light", 40))
+        self.root.mainloop()
 
-        self.label = QLabel("Press 'Enter' to start selecting Game Mode")
+    def reset(self):
 
-        layout.addWidget(self.label)
 
-        self.setLayout(layout)
-        self.setGeometry(100, 100, 400, 200)
-        self.setWindowTitle('Game Selector')
-        self.show()
 
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Enter or event.key() == Qt.Key_Return:
-            self.select_next()
-        elif event.key() == Qt.Key_Backspace:
-            self.goBack()
+        self.pressedKeys = []
 
-    def select_next(self):
-        if self.selection_index == 0:
-            self.select_game_mode()
-        elif self.selection_index == 1:
-            self.select_difficulty()
-        elif self.selection_index == 2:
-            self.select_time()
+        self.current = 0
+        self.selection = -1
 
-    def select_game_mode(self):
-        game_mode, ok = GameSelectorDialog.get_selection("Select Game Mode", self.game_modes)
-        if ok:
-            self.selected_game_mode = game_mode
-            self.selection_index += 1
-            self.update_label()
 
-    def select_difficulty(self):
-        difficulty, ok = GameSelectorDialog.get_selection("Select Difficulty", self.difficulties)
-        if ok:
-            self.selected_difficulty = difficulty
-            self.selection_index += 1
-            self.update_label()
+        tk.Label(self.root, name="text", text="Spielmodus:", bg="black", fg="green", font=("Ubuntu", 50)).pack()
 
-    def select_time(self):
-        time, ok = GameSelectorDialog.get_selection("Select Time", self.times)
-        if ok:
-            self.selected_time = time
-            self.update_label()
+        tk.Label(self.root, name="lable1", text="1:Bombe", bg="black", fg="green", font=("Ubuntu", 45)).place(x=150,
+                                                                                                              y=120)
+        tk.Label(self.root, name="lable2", text="2:Bunker", bg="black", fg="green", font=("Ubuntu", 45)).place(x=150,
+                                                                                                               y=220)
+        tk.Label(self.root, name="lable3", text="3:Flage", bg="black", fg="green", font=("Ubuntu", 45)).place(x=150,
+                                                                                                              y=320)
+        self.root.attributes("-fullscreen", True)
+        tk.Label(self.root, name="dont1", text="Rot: Zurück", fg="green", bg="black", font=("Ubuntu", 30)).place(
+            relx=0.0, rely=1.0, anchor='sw')
+        tk.Label(self.root, name="dont2", text="Blau: Bestätigen", fg="green", bg="black", font=("Ubuntu", 30)).place(
+            relx=1.0, rely=1.0, anchor='se')
 
-    def goBack(self):
-        if self.selection_index > 0:
-            self.selection_index -= 1
-        self.update_label()
+        self.selectLable = tk.Label(self.root, text="<--", bg="black", fg="green", font=("calibri light", 40))
 
-    def update_label(self):
-        if self.selection_index == 0:
-            text = "Press 'Enter' to select Game Mode"
-        elif self.selection_index == 1:
-            text = f"Selected Game Mode: {self.selected_game_mode}\nPress 'Enter' to select Difficulty"
-        elif self.selection_index == 2:
-            text = f"Selected Game Mode: {self.selected_game_mode}\nSelected Difficulty: {self.selected_difficulty}\nPress 'Enter' to select Time"
+    def keydown(self, key):
+        if key.keysym not in self.pressedKeys:
+            self.pressedKeys.append(key.keysym)
+            self.gameSelectInput(key)
+
+    def keyup(self, key):
+        if key.keysym in self.pressedKeys:
+            self.pressedKeys.remove(key.keysym)
+
+    def gameSelectInput(self, key):
+        print(key.keysym)
+        if not self.isInGame:
+            if key.keysym in ["1", "2", "3"]:
+                if self.selectedGame is None:
+                    print("Game")
+                    self.selection = int(key.char) - 1
+                    self.selectLable.configure(text="<--")
+                    self.selectLable.place(x=160 + self.getLableWidght(key)[0], y=self.hoehe[int(key.char) - 1])
+                elif self.selectedDiff is None:
+                    print("diff")
+                    self.selection = int(key.char) - 1
+                    self.selectLable.configure(text="<--")
+                    self.selectLable.place(x=160 + self.getLableWidght(key)[0], y=self.hoehe[int(key.char) - 1])
+                elif self.selectedTime is None:
+                    print("time")
+                    self.selection = int(key.char) - 1
+                    self.selectLable.configure(text="<--")
+                    self.selectLable.place(x=160 + self.getLableWidght(key)[0], y=self.hoehe[int(key.char) - 1])
+            elif key.keysym == "Return" and self.selection in range(0, 3):
+                if self.current == 0:
+                    self.selectLable.place(x=1111, y=20000)
+                    self.setLables(["1:Easy", "2:Medium", "3:Hard", "Schrigkeit"])
+                    self.selectedGame = self.modie[self.selection]
+                    self.selection = -1
+                    self.current = self.current + 1
+                elif self.current == 1:
+                    self.selectLable.place(x=1000,y=213123)
+                    self.setLables(["1:5Min", "2:10Min", "3:15Min", "Zeit"])
+                    self.selectedDiff = self.diffs[self.selection]
+                    self.selection = -1
+                    self.current = self.current + 1
+                elif self.current == 2:
+                    self.selectLable.place(x=1000,y=123123)
+                    self.selectedTime = self.times[self.selection]
+                    print(self.selectedGame, self.selectedDiff, self.selectedTime)
+                    self.clearFrame()
+                    self.startGame()
+            elif key.keysym == "Delete":
+                if self.current == 1:
+                    self.setLables(["1:Bomb", "2:Bunker", "3:Flag", "Modus:"])
+                    self.current = self.current - 1
+                    self.selection = -1
+                    self.selectLable.place(x=1000,y=1200)
+                    self.selectedGame = None
+                elif self.current == 2:
+                    self.setLables(["1:Easy", "2:Medium", "3:Hard", "Schrigkeit"])
+                    self.current = self.current - 1
+                    self.selection = -1
+                    self.selectLable.place(x=1000,y=123123)
+                    self.selectedDiff = None
         else:
-            text = f"Selected Game Mode: {self.selected_game_mode}\nSelected Difficulty: {self.selected_difficulty}\nSelected Time: {self.selected_time}"
+            if self.selectedGame == "Bombe":
+                if key.keysym == "Delete":
+                    self.input = []
+                    self.inputLable.configure(text="")
+                elif key.keysym == "Return":
+                    if "".join(self.input) == self.armCode:
+                        self.defuseBomb()
+                else:
+                    self.input.append(key.keysym)
+                    self.inputLable.configure(text="".join(self.input))
 
-        self.label.setText(text)
 
-class GameSelectorDialog(QWidget):
-    @staticmethod
-    def get_selection(title, options):
-        selection, ok = GameSelectorDialog.show_dialog(title, options)
-        return selection, ok
 
-    @staticmethod
-    def show_dialog(title, options):
-        dialog = GameSelectorDialog(title, options)
-        result = dialog.exec_()
-        return dialog.selected_item, result == dialog.Accepted
 
-    def __init__(self, title, options):
-        super().__init__()
+    def startGame(self):
+        if self.selectedGame == "Bombe":
+            self.isInGame = True
+            self.armCode = codeGen(1)
+            self.defCode = codeGen(1)
+            self.armBomb()
+            self.armTries = 3
+        elif self.selectedGame == "Bunker":
+            self.isInGame = True
+            self.bunker()
+        elif self.selectedGame == "Flage":
+            self.isInGame = True
+            self.flage()
 
-        self.selected_item = None
+    def clearFrame(self):
+        for wi in self.root.winfo_children():
+            wi.destroy()
 
-        self.init_ui(title, options)
+    def clearFrameCarfull(self):
+        for wi in self.root.winfo_children():
+            if str(wi) not in [".dont1", ".dont2"]:
+                wi.destroy()
 
-    def init_ui(self, title, options):
-        layout = QVBoxLayout()
+    def setLables(self, x):
+        for wi in self.root.winfo_children():
+            if str(wi) == ".lable1":
+                wi.configure(text=x[0])
+            elif str(wi) == ".lable2":
+                wi.configure(text=x[1])
+            elif str(wi) == ".lable3":
+                wi.configure(text=x[2])
+            elif str(wi) == ".text":
+                wi.configure(text=x[3])
 
-        self.label = QLabel(title)
-        layout.addWidget(self.label)
+    def getLableWidght(self, key):
+        if key.char == '1':
+            for wi in self.root.winfo_children():
+                if str(wi) == ".lable1":
+                    return [wi.winfo_width(), wi.winfo_height()]
+        if key.char == '2':
+            for wi in self.root.winfo_children():
+                if str(wi) == ".lable2":
+                    return [wi.winfo_width(), wi.winfo_height()]
+        if key.char == '3':
+            for wi in self.root.winfo_children():
+                if str(wi) == ".lable3":
+                    return [wi.winfo_width(), wi.winfo_height()]
 
-        self.list_widget = QLabel("\n".join(options))
-        layout.addWidget(self.list_widget)
+    def armBomb(self):
+        self.clearFrame()
+        tk.Label(self.root, fg="green", bg="black", text="Bombe scharf stellen", font=("Ubuntu", 50)).pack()
+        tk.Label(self.root, fg="green", bg="black", text="Code:", font=("Ubuntu", 15)).place(x=20, y=160)
+        tk.Label(self.root, fg="green", bg="black", text=self.armCode, font=("Ubuntu", 15)).place(x=100, y=160)
+        tk.Label(self.root, fg="green", bg="black", text="Eingabe:", font=("Ubuntu", 30)).place(x=10, y=220)
+        tk.Label(self.root, fg="green", bg="black", text="Versuche:", font=("Ubuntu", 30)).place(x=10, y=280)
+        self.versucheLable = tk.Label(self.root, fg="green", bg="black", text=str(self.armTries), font=("Ubuntu", 30))
+        self.versucheLable.place(x=210, y=280)
 
-        self.setLayout(layout)
-        self.setGeometry(300, 300, 200, 200)
-        self.setWindowTitle(title)
+        self.inputLable = tk.Label(self.root, fg="green", bg="black", text="", font=("Ubuntu", 30))
+        self.inputLable.place(x=180, y=220)
 
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Up:
-            self.move_selection(-1)
-        elif event.key() == Qt.Key_Down:
-            self.move_selection(1)
-        elif event.key() == Qt.Key_Return:
-            self.accept()
-        elif event.key() == Qt.Key_Backspace:
-            self.reject()
+        self.infoLable = tk.Label(self.root, fg="green", bg="black", text="", font=("Ubuntu", 30))
+        self.infoLable.place(x=10, y=350)
+        print("armbomb")
 
-    def move_selection(self, direction):
-        current_row = self.list_widget.text().split('\n').index(self.selected_item) if self.selected_item else -1
-        new_row = (current_row + direction) % len(self.list_widget.text().split('\n'))
-        self.selected_item = self.list_widget.text().split('\n')[new_row]
+    def defuseBomb(self):
+        print("test2")
+        self.clearFrame()
+        self.timerLable = tk.Label(self.root, fg="green", bg="black", text="", font=("Ubuntu", 50))
+        self.timerLable.pack()
+        tk.Label(self.root, fg="green", bg="black", text="Code:", font=("Ubuntu", 15)).place(x=20, y=160)
+        tk.Label(self.root, fg="green", bg="black", text=self.defCode, font=("Ubuntu", 15)).place(x=100, y=160)
+        tk.Label(self.root, fg="green", bg="black", text="Eingabe:", font=("Ubuntu", 30)).place(x=10, y=220)
+        tk.Label(self.root, fg="green", bg="black", text="Versuche:", font=("Ubuntu", 30)).place(x=10, y=280)
+        self.versucheLable = tk.Label(self.root, fg="green", bg="black", text=str(self.bomTries), font=("Ubuntu", 30))
+        self.versucheLable.place(x=210, y=280)
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = GameSelector()
-    sys.exit(app.exec_())
+        self.inputLable = tk.Label(self.root, fg="green", bg="black", text="", font=("Ubuntu", 30))
+        self.inputLable.place(x=180, y=220)
+
+        self.infoLable = tk.Label(self.root, fg="green", bg="black", text="", font=("Ubuntu", 30))
+        self.infoLable.place(x=10, y=350)
+        self.mp = threading.Thread(target=self.timer)
+        self.mp.start()
+
+
+    def timer(self):
+        ctime = self.selectedTime * 60
+        while ctime and not self.stop:
+            mins, secs = divmod(ctime, 60)
+            timeformat = '{:02d}:{:02d}'.format(mins, secs)
+            self.timerLable.configure(text=timeformat)
+            time.sleep(1)
+            ctime -= 1
+
+#time.sleep(4)
+tmp = logicWindow()
